29eec5b778d1fe3ca35659a407a8c6bc96a8d36b
today8934 <43538380+today8934@users.noreply.github.com>
2025-09-08 19:46:10 +0900

    Time: 101 ms (5.86%) | Memory: 45.4 MB (12.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,7 +3,6 @@ class Solution {
         int max = 0;
         for (int i = 0; i < s.length(); i++) {
             char curr = s.charAt(i);
-
             Set<Character> set = new HashSet<>();
 
             int index = i;
